6ff0088cbdb8f74ca3fdc6b67ae69042d0bda356
Ümit Kengil <umitkngl@gmail.com>
2022-01-01 14:27:30 +0300

    Add files via upload

---
+++ CSharp-101/degisken/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace degisken
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str1 = string.Empty;
+
+            str1 = "abcdef";
+
+            string name = "abc";
+            string surname = "abcdf";
+            string fullname = name + " " + surname;
+            int integer1 = 10;
+            int integer2 = 20;
+            int integer3 = integer1 + integer2;
+
+            bool bool1 = 10>2; // True
+            string str20 = "20";
+            int int20 = 20;
+
+            string yeniDeger = str20 + int20.ToString();
+            Console.WriteLine("Yeni Deger: "+yeniDeger);
+
+            int int21 = int20 + Convert.ToInt32(str20);
+            Console.WriteLine("int21 Convert Edilen Degeri: "+int21);
+
+            int int22 = int20 + int.Parse(str20);
+            Console.WriteLine("int22 Parse Edilen Degeri: "+int21);
+
+            DateTime date = DateTime.Now;
+            Console.WriteLine(date);
+
+            string datetime = DateTime.Now.ToString("dd.MM.yyyy");
+            Console.WriteLine(datetime);
+
+        }
+    }
+}
